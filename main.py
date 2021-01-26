a0ad108da38b7c0f59f1e810a0ad7e258a9af618
AlirezaShamsoshoara <a.shamsoshoara@gmail.com>
2021-01-26 09:55:23 -0700

    add main file

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Created on Mar. 20, 2020
+@author:    Alireza Shamsoshoara
+@Project:   Imitation Learning (Behavioral-Cloning) for UAV-Assisted Communication in Remote Disaster Area
+            Paper: ### TODO
+            Arxiv: ### TODO
+            YouTube Link for the designed Simulator: https://youtu.be/xYSlZac-AMM
+@Northern Arizona University
+This project is developed and tested with Python 3.6 using pycharm on Ubuntu 18.04 LTS machine
+"""
