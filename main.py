44ed3cb9984e47587a82127ba22280ce08de2bd8
Alireza <a.shamsoshoara@gmail.com>
2021-01-26 10:12:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 """
-Created on Mar. 20, 2020
+Created on January 26th, 2020
 @author:    Alireza Shamsoshoara
-@Project:   Imitation Learning (Behavioral-Cloning) for UAV-Assisted Communication in Remote Disaster Area
+@Project:   UAV communication using apprenticeship learning via Inverse Reinforcement Learning (IRL)
             Paper: ### TODO
             Arxiv: ### TODO
-            YouTube Link for the designed Simulator: https://youtu.be/xYSlZac-AMM
+            YouTUbeLink: ### TODO
 @Northern Arizona University
 This project is developed and tested with Python 3.6 using pycharm on Ubuntu 18.04 LTS machine
 """
+
+print("Hello")
