7b49375722bec495b5a104025228d13c4878aef0
alireza <a.shamsoshoara@gmail.com>
2021-01-28 17:36:30 -0700

    Update location.py

--- location.py
+++ location.py
@@ -8,6 +8,7 @@
 # import libraries
 import numpy as np
 import matplotlib.pyplot as plt
+from hexalattice.hexalattice import *
 from matplotlib.patches import RegularPolygon
 
 #########################################################
@@ -20,9 +21,15 @@ from matplotlib.patches import RegularPolygon
 def plothexagon():
     fig, ax = plt.subplots(1)
     ax.set_aspect('equal')
-    hexagon = RegularPolygon((0, 0), numVertices=6, radius=1, edgecolor='k', facecolor='g', alpha=0.4)
+    hexagon = RegularPolygon((0, 0), numVertices=6, radius=1, edgecolor='k', facecolor='g', alpha=0.3,
+                             orientation=np.float(np.radians(30)))
     ax.add_patch(hexagon)
 
+    hex_centers, _ = create_hex_grid(nx=5,
+                                     ny=5,
+                                     do_plot=True)
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([-1, 1])
     plt.show()
 
 
