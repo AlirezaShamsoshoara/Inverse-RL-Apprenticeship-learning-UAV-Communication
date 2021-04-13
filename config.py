4238bc19d2407bee5bc3bdfdd2f6570b013d3cb2
alireza <a.shamsoshoara@gmail.com>
2021-04-12 19:10:39 -0700

    Update config.py

--- config.py
+++ config.py
@@ -15,6 +15,11 @@ Mode = 'BC'
 # Different Modes {"Expert", "IRL_SGD", "IRL_DQN", "DRL", "QRL", "BC", "Shortest", "Random", "ResultsIRL", "ResultsDRL",
 # "ResultsQRL", "ResultsBC", "ResultShortest", "ResultsRand"}
 
+Config_Flags = {'SAVE_path': True, 'Display_map': False, 'SingleArrow': False, 'SAVE_IRL_DATA': False,
+                'SAVE_EXPERT_DATA': True, 'SAVE_IRL_WEIGHT': False, 'SAVE_MODEL_IRL_SGD': False, 'PLOT_RESULTS': True,
+                'SAVE_PLOT_PDF': False, 'SAVE_PLOT_FIG': False, 'PRINT_INFO': True, 'LOAD_IRL': False,
+                'SAVE_DATA_BC_EXPERT': True, 'SAVE_MODEL_BC': True}
+
 # Possible number of UEs Cluster: 75
 # Possible number of Cells: 25
 Config_General = {'NUM_UAV': 1, 'Size': 5, 'NUM_CELLS': 25, 'NUM_UEs': 75, 'Radius': 10, 'Loc_delta': 2,
@@ -23,19 +28,13 @@ Config_requirement = {'dist_limit': Config_General.get('Size') + 3, 'MAX_DISTANC
                       'MAX_UE_NEIGHBORS': 29, 'MIN_INTERFERENCE': 0.5123281666343314,
                       'MAX_INTERFERENCE': 14.621335028196711}
 
-movement_actions_list = [1, 2, 3, 4, 5, 6]  # 1: North, 2: North East, 3: South East, 4: South, 5: South West,
-# 6: North West
 Number_of_neighbor_UEs = {'Min': 0, 'Max': 0}
-
 
 config_movement_step = {'x_step': (Config_General.get('Radius')) * (3./2.),
                         'y_step': (Config_General.get('Radius')) * np.sqrt(3)}
 
-Config_Flags = {'SAVE_path': True, 'Display_map': False, 'SingleArrow': False, 'SAVE_IRL_DATA': False,
-                'SAVE_EXPERT_DATA': True, 'SAVE_IRL_WEIGHT': False, 'SAVE_MODEL_IRL_SGD': False, 'PLOT_RESULTS': True,
-                'SAVE_PLOT_PDF': False, 'SAVE_PLOT_FIG': False, 'PRINT_INFO': False, 'LOAD_IRL': False,
-                'SAVE_DATA_BC_EXPERT': True, 'SAVE_MODEL_BC': True}
-
+movement_actions_list = [1, 2, 3, 4, 5, 6]  # 1: North, 2: North East, 3: South East, 4: South, 5: South West,
+# 6: North West
 Config_interference = {'AntennaGain': 100, 'Bandwidth': 50}
 Config_Power = {'UE_Tr_power': 2.0, 'UAV_Tr_power': [50.0, 60.0, 80.0, 100.0, 150.0, 200.0], 'UAV_init_energy': 400.0,
                 'UAV_mobility_consumption': 10.0}  # Tr power: mW, Energy, Jule
@@ -47,7 +46,7 @@ Config_IRL = {'NUM_FEATURES': 5, 'NUM_EPOCHS': 10002, 'NUM_PLAY': 1, 'NUM_TRAJEC
               'EPSILON_OPTIMIZATION': 0.01, 'LEARNING_RATE': 1e-3, 'BATCH_SIZE': 100, 'EPSILON_GREEDY': 0.1,
               'GAMMA_DISCOUNT': 0.9}
 
-Config_BehavioralCloning = {'NUM_TRAJECTORIES_EXPERT': 100000}
+Config_BehavioralCloning = {'NUM_TRAJECTORIES_EXPERT': 10000}
 
 Config_QRL = {}
 Config_DRL = {}
@@ -62,6 +61,7 @@ ResultPathPDF = "Results/PDF/"
 ResultPathFIG = "Results/FIG/"
 SGDModelPath = "Data/InverseRL/SGDModel/"
 ExpertPath_BC = "Data/BehavioralCloning/"
+BCModelPath = "Data/BehavioralCloning/Model/"
 Config_Path = {'PathDist': pathDist, 'ExpertPath': ExpertPath, 'WeightPath': WeightPath, 'InverseRLPath': InverseRLPath,
                'ResultPathPDF': ResultPathPDF, 'ResultPathFIG': ResultPathFIG, 'SGDModelPath': SGDModelPath,
-               'ExpertPath_BC': ExpertPath_BC}
+               'ExpertPath_BC': ExpertPath_BC, 'BCModelPath': BCModelPath}
