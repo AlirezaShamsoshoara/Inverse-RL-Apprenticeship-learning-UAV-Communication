67990d001e189f44c409d4a155bb0a5f976cebdc
alireza <a.shamsoshoara@gmail.com>
2021-04-21 15:40:33 -0700

    Update plotresults.py

--- plotresults.py
+++ plotresults.py
@@ -377,4 +377,100 @@ def plot_sample_trajectories(trajectories_sgd, trajectories_dqn, trajectories_bc
 
 
 def plot_error_trajectories(trajectories_sgd, trajectories_dqn, trajectories_bc, cell_objects):
-    pass
+    num_steps_sgd = len(trajectories_sgd[0])
+    num_steps_dqn = len(trajectories_dqn[0])
+    num_steps_bc = len(trajectories_bc[0])
+
+    throughput_sgd = np.zeros(num_steps_sgd, dtype=float)
+    interference_sgd = np.zeros(num_steps_sgd, dtype=float)
+    distance_sgd = np.zeros(num_steps_sgd, dtype=int)
+
+    throughput_dqn = np.zeros(num_steps_dqn, dtype=float)
+    interference_dqn = np.zeros(num_steps_dqn, dtype=float)
+    distance_dqn = np.zeros(num_steps_dqn, dtype=int)
+
+    throughput_bc = np.zeros(num_steps_bc, dtype=float)
+    interference_bc = np.zeros(num_steps_bc, dtype=float)
+    distance_bc = np.zeros(num_steps_bc, dtype=int)
+
+    next_spec_index = 4
+    throughput_index = 2
+    interference_ue_index = 3
+    new_cell_index = 6
+
+    for step, trajectory in enumerate(trajectories_sgd[0]):
+        throughput_sgd[step] = trajectory[next_spec_index][throughput_index]
+        interference_sgd[step] = trajectory[next_spec_index][interference_ue_index]
+        distance_sgd[step] = cell_objects[trajectory[new_cell_index]].get_distance()
+
+    for step, trajectory in enumerate(trajectories_dqn[0]):
+        throughput_dqn[step] = trajectory[next_spec_index][throughput_index]
+        interference_dqn[step] = trajectory[next_spec_index][interference_ue_index]
+        distance_dqn[step] = cell_objects[trajectory[new_cell_index]].get_distance()
+
+    for step, trajectory in enumerate(trajectories_bc[0]):
+        throughput_bc[step] = trajectory[next_spec_index][throughput_index]
+        interference_bc[step] = trajectory[next_spec_index][interference_ue_index]
+        distance_bc[step] = cell_objects[trajectory[new_cell_index]].get_distance()
+
+    fig_error_throughput = plt.figure(figsize=(8, 8))
+    ax_error_throughput = fig_error_throughput.add_subplot(111)
+    labels = ['Q-Learning', 'DQN', 'BC']
+    x_pos = np.arange(len(labels))
+    means = [np.mean(throughput_sgd), np.mean(throughput_dqn), np.mean(throughput_bc)]
+    errors = [np.std(throughput_sgd), np.std(throughput_dqn), np.std(throughput_bc)]
+    ax_error_throughput.bar(x_pos, means, width=0.7, yerr=errors, align='center', alpha=0.5, ecolor='black',
+                             capsize=10, color=['b', 'r', 'g', 'k', 'y', 'm', 'c'])
+    ax_error_throughput.set_xticks(x_pos)
+    ax_error_throughput.set_xticklabels(labels, size=14, fontweight='bold')
+    ax_error_throughput.set_ylabel('Average UAV throughput - up link (Mbps)', size=14, fontweight='bold')
+    ax_error_throughput.set_title('Throughput mean and std with environmental error', size=14, fontweight='bold')
+    ax_error_throughput.yaxis.grid(True)
+
+    file_fig_obj_throughput = ResultPathFIG + 'Throughput_error_steps_%d.fig.pickle' % \
+                              num_steps_sgd
+    file_fig_pdf_throughput = ResultPathPDF + 'Throughput_error_steps_%d.pdf' % \
+                              num_steps_sgd
+
+    fig_error_interference = plt.figure(figsize=(8, 8))
+    ax_error_interference = fig_error_interference.add_subplot(111)
+    means_interference = [np.mean(interference_sgd), np.mean(interference_dqn), np.mean(interference_bc)]
+    errors_interference = [np.std(interference_sgd), np.std(interference_dqn), np.std(interference_bc)]
+    ax_error_interference.bar(x_pos, means_interference, width=0.7, yerr=errors_interference, align='center',
+                               alpha=0.5, ecolor='black', capsize=10, color=['b', 'r', 'g', 'k', 'y', 'm', 'c'])
+    ax_error_interference.set_xticks(x_pos)
+    ax_error_interference.set_xticklabels(labels, size=14, fontweight='bold')
+    ax_error_interference.set_ylabel('Average UAV interference on UEs DL', size=14, fontweight='bold')
+    ax_error_interference.set_title('Interference mean and std with environmental error', size=14, fontweight='bold')
+    ax_error_interference.yaxis.grid(True)
+
+    file_fig_obj_interference = ResultPathFIG + 'Interference_error_steps_%d.fig.pickle' % \
+                                num_steps_sgd
+    file_fig_pdf_interference = ResultPathPDF + 'Interference_error_steps_%d.pdf' % \
+                                num_steps_sgd
+
+    fig_error_distance = plt.figure(figsize=(8, 8))
+    ax_error_distance = fig_error_distance.add_subplot(111)
+    ax_error_distance.set_xlabel("Steps", size=14, fontweight='bold')
+    ax_error_distance.set_ylabel("Minimum distance to the destination", size=14, fontweight='bold')
+    ax_error_distance.plot(np.arange(0, num_steps_sgd) + 1.15, distance_sgd, color="blue",
+                            linestyle='solid', marker='o', markersize='5', label='Q-Learning', linewidth=1.5)
+    ax_error_distance.plot(np.arange(0, num_steps_dqn) + 0.85, distance_dqn, color="red",
+                            linestyle='solid', marker='x', markersize='5', label='DQN', linewidth=1.5)
+    ax_error_distance.plot(np.arange(0, num_steps_bc) + 1, distance_bc, color="green",
+                            linestyle='solid', marker='D', markersize='5', label='BC', linewidth=1.5)
+    ax_error_distance.grid(True)
+    ax_error_distance.legend(prop={'size': 14, 'weight': 'bold'}, loc='best')
+
+    file_fig_obj_distance = ResultPathFIG + 'Distance_error_steps_%d.fig.pickle' % \
+                            num_steps_sgd
+    file_fig_pdf_distance = ResultPathPDF + 'Distance_error_steps_%d.pdf' % \
+                            num_steps_sgd
+    if Config_Flags.get("SAVE_PLOT_PDF"):
+        fig_error_throughput.savefig(file_fig_pdf_throughput, bbox_inches='tight')
+        fig_error_interference.savefig(file_fig_pdf_interference, bbox_inches='tight')
+        fig_error_distance.savefig(file_fig_pdf_distance, bbox_inches='tight')
+    if Config_Flags.get("SAVE_PLOT_FIG"):
+        pickle.dump(fig_error_throughput, open(file_fig_obj_throughput, 'wb'))
+        pickle.dump(fig_error_interference, open(file_fig_obj_interference, 'wb'))
+        pickle.dump(fig_error_distance, open(file_fig_obj_distance, 'wb'))
